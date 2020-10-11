f62172742624e2a157c23646edd1736ff08ca89d
TheNootLinja <thenootlinja@gmail.com>
2020-10-11 17:06:39 -0400

    Removed the commented out array of meetups from app.svlete

--- src/App.svelte
+++ src/App.svelte
@@ -8,29 +8,6 @@
 
   let editMode;
 
-  // let meetups = [
-  //   {
-  //     id: "m1",
-  //     title: "Coding Bootcamp",
-  //     subtitle: "Learn to code in 2 hours",
-  //     description:
-  //       "In this meetup, we will have some experts to teach you how to code",
-  //     image: "https://picsum.photos/1000/1000",
-  //     address: "27th Nerd Road, 32523 New York",
-  //     contactEmail: "code@test.com",
-  //     isFavorite: false,
-  //   },
-  //   {
-  //     id: "m2",
-  //     title: "Svelte Together",
-  //     subtitle: "Let's learn Svelte together!",
-  //     description: "Just another description for our date",
-  //     image: "https://picsum.photos/1000/1000",
-  //     address: "29th Nerd Road, 32523 New York",
-  //     contactEmail: "code@test.com",
-  //     isFavorite: false,
-  //   },
-
   function addMeetup() {
     editMode = null;
   }
